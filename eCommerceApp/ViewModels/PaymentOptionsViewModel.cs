db4f4a01f6b58d5d49feec3660d1ce143bd70866
zeynep özge dirikgil <zodirikgil@gmail.com>
2024-09-03 10:53:15 +0300

    Builded.

--- eCommerceApp/ViewModels/PaymentOptionsViewModel.cs
+++ eCommerceApp/ViewModels/PaymentOptionsViewModel.cs
@@ -2,6 +2,10 @@
 {
     public class PaymentOptionsViewModel
     {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
         public decimal? TotalPrice { get; set; }
         public IEnumerable<Models.PaymentOptions> PaymentOptions { get; set; }
     }
